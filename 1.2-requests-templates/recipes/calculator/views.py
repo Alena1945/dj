e8cf5a6f015de9907ea5f1d5303841ef62ebce11
Alena1945 <alena.belousova.2811@mail.ru>
2022-10-05 18:59:51 +0500

    Initial commit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,8 +16,49 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    'cutlet': {
+        'куринный фарш, г': 400,
+        'яйцо, шт': 1,
+        'лук, шт': 1,
+        'чеснок, шт': 1,
+        'приправы, ч.л': 1.5,
+        'зелень, г': 15,
+    }
 }
+
+
+def calculate_recipe_view(request, recipe_name):
+    if recipe_name in DATA:
+        data = DATA[recipe_name]
+        servings = request.GET.get('servings', None)
+
+        if servings:
+            result = dict()
+            for item, value in data.items():
+                new_value = value * int(servings)
+                result[item] = new_value
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': result
+            }
+        else:
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': data
+            }
+    else:
+        context = None
+
+    return render(request, template_name='calculator/index.html', context=context)
+
+
+def home_view(request):
+    all_recipes = list(DATA.keys())
+    context = {'all_recipes': all_recipes}
+
+    return render(request, template_name='home/home.html', context=context)
+
+
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
